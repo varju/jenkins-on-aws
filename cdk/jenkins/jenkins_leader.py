a3762164bc69b2f638a3fc8a569ee413149c1868
Alex Varju <alex@varju.ca>
2024-05-30 15:15:52 -0700

    Drop EC2 variant

--- cdk/jenkins/jenkins_leader.py
+++ cdk/jenkins/jenkins_leader.py
@@ -8,9 +8,6 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_servicediscovery as sd,
     aws_iam as iam,
-    aws_logs as logs,
-    aws_elasticloadbalancingv2 as elb,
-    Duration,
     Stack,
 )
 from constructs import Construct
@@ -33,131 +30,48 @@ class JenkinsLeader(Stack):
             directory='./docker/leader/'
         )
 
-        if config['DEFAULT']['fargate_enabled'] == "yes" or not config['DEFAULT']['ec2_enabled'] == "yes":
-            # Task definition details to define the Jenkins leader container
-            self.jenkins_task = ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
-                image=ecs.ContainerImage.from_docker_image_asset(self.container_image),
-                container_port=8080,
-                enable_logging=True,
-                environment={
-                    # https://github.com/jenkinsci/docker/blob/leader/README.md#passing-jvm-parameters
-                    'JAVA_OPTS': '-Djenkins.install.runSetupWizard=false',
-                    # https://github.com/jenkinsci/configuration-as-code-plugin/blob/leader/README.md#getting-started
-                    'CASC_JENKINS_CONFIG': '/config-as-code.yaml',
-                    'network_stack': self.vpc.stack_name,
-                    'cluster_stack': self.cluster.stack_name,
-                    'worker_stack': self.worker.stack_name,
-                    'cluster_arn': self.cluster.cluster.cluster_arn,
-                    'aws_region': config['DEFAULT']['region'],
-                    'jenkins_url': config['DEFAULT']['jenkins_url'],
-                    'subnet_ids': ",".join([x.subnet_id for x in self.vpc.vpc.private_subnets]),
-                    'security_group_ids': self.worker.worker_security_group.security_group_id,
-                    'execution_role_arn': self.worker.worker_execution_role.role_arn,
-                    'task_role_arn': self.worker.worker_task_role.role_arn,
-                    'worker_log_group': self.worker.worker_logs_group.log_group_name,
-                    'worker_log_stream_prefix': self.worker.worker_log_stream.log_stream_name,
-                    'admin_username': os.environ['ADMIN_USERNAME'],
-                    'admin_password': os.environ['ADMIN_PASSWORD'],
-                    'TZ': 'America/Vancouver',
-                },
-            )
+        # Task definition details to define the Jenkins leader container
+        self.jenkins_task = ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+            image=ecs.ContainerImage.from_docker_image_asset(self.container_image),
+            container_port=8080,
+            enable_logging=True,
+            environment={
+                # https://github.com/jenkinsci/docker/blob/leader/README.md#passing-jvm-parameters
+                'JAVA_OPTS': '-Djenkins.install.runSetupWizard=false',
+                # https://github.com/jenkinsci/configuration-as-code-plugin/blob/leader/README.md#getting-started
+                'CASC_JENKINS_CONFIG': '/config-as-code.yaml',
+                'network_stack': self.vpc.stack_name,
+                'cluster_stack': self.cluster.stack_name,
+                'worker_stack': self.worker.stack_name,
+                'cluster_arn': self.cluster.cluster.cluster_arn,
+                'aws_region': config['DEFAULT']['region'],
+                'jenkins_url': config['DEFAULT']['jenkins_url'],
+                'subnet_ids': ",".join([x.subnet_id for x in self.vpc.vpc.private_subnets]),
+                'security_group_ids': self.worker.worker_security_group.security_group_id,
+                'execution_role_arn': self.worker.worker_execution_role.role_arn,
+                'task_role_arn': self.worker.worker_task_role.role_arn,
+                'worker_log_group': self.worker.worker_logs_group.log_group_name,
+                'worker_log_stream_prefix': self.worker.worker_log_stream.log_stream_name,
+                'admin_username': os.environ['ADMIN_USERNAME'],
+                'admin_password': os.environ['ADMIN_PASSWORD'],
+                'TZ': 'America/Vancouver',
+            },
+        )
 
-            # Create the Jenkins leader service
-            self.jenkins_leader_service_main = ecs_patterns.ApplicationLoadBalancedFargateService(
-                self, "JenkinsleaderService",
-                cpu=int(config['DEFAULT']['fargate_cpu']),
-                memory_limit_mib=int(config['DEFAULT']['fargate_memory_limit_mib']),
-                cluster=self.cluster.cluster,
-                desired_count=1,
-                enable_ecs_managed_tags=True,
-                task_image_options=self.jenkins_task,
-                cloud_map_options=ecs.CloudMapOptions(name="leader", dns_record_type=sd.DnsRecordType('A'))
-            )
+        # Create the Jenkins leader service
+        self.jenkins_leader_service_main = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "JenkinsleaderService",
+            cpu=int(config['DEFAULT']['fargate_cpu']),
+            memory_limit_mib=int(config['DEFAULT']['fargate_memory_limit_mib']),
+            cluster=self.cluster.cluster,
+            desired_count=1,
+            enable_ecs_managed_tags=True,
+            task_image_options=self.jenkins_task,
+            cloud_map_options=ecs.CloudMapOptions(name="leader", dns_record_type=sd.DnsRecordType('A'))
+        )
 
-            self.jenkins_leader_service = self.jenkins_leader_service_main.service
-            self.jenkins_leader_task = self.jenkins_leader_service.task_definition
-
-        if config['DEFAULT']['ec2_enabled'] == "yes":
-            self.jenkins_load_balancer = elb.ApplicationLoadBalancer(
-                self, "JenkinsleaderELB",
-                vpc=self.vpc.vpc,
-                internet_facing=True,
-            )
-
-            self.listener = self.jenkins_load_balancer.add_listener("Listener", port=80)
-
-            self.jenkins_leader_task = ecs.Ec2TaskDefinition(
-                self, "JenkinsleaderTaskDef",
-                network_mode=ecs.NetworkMode.AWS_VPC,
-                volumes=[ecs.Volume(name="efs_mount", host=ecs.Host(source_path='/mnt/efs'))],
-            )
-
-            self.jenkins_leader_task.add_container(
-                "JenkinsleaderContainer",
-                image=ecs.ContainerImage.from_ecr_repository(self.container_image.repository),
-                cpu=int(config['DEFAULT']['ec2_cpu']),
-                memory_limit_mib=int(config['DEFAULT']['ec2_memory_limit_mib']),
-                environment={
-                    # https://github.com/jenkinsci/docker/blob/leader/README.md#passing-jvm-parameters
-                    'JAVA_OPTS': '-Djenkins.install.runSetupWizard=false',
-                    # https://github.com/jenkinsci/configuration-as-code-plugin/blob/leader/README.md#getting-started
-                    'CASC_JENKINS_CONFIG': '/config-as-code.yaml',
-                    'network_stack': self.vpc.stack_name,
-                    'cluster_stack': self.cluster.stack_name,
-                    'worker_stack': self.worker.stack_name,
-                    'cluster_arn': self.cluster.cluster.cluster_arn,
-                    'aws_region': config['DEFAULT']['region'],
-                    'jenkins_url': config['DEFAULT']['jenkins_url'],
-                    'subnet_ids': ",".join([x.subnet_id for x in self.vpc.vpc.private_subnets]),
-                    'security_group_ids': self.worker.worker_security_group.security_group_id,
-                    'execution_role_arn': self.worker.worker_execution_role.role_arn,
-                    'task_role_arn': self.worker.worker_task_role.role_arn,
-                    'worker_log_group': self.worker.worker_logs_group.log_group_name,
-                    'worker_log_stream_prefix': self.worker.worker_log_stream.log_stream_name
-                },
-                logging=ecs.LogDriver.aws_logs(
-                    stream_prefix="Jenkinsleader",
-                    log_retention=logs.RetentionDays.ONE_WEEK
-                ),
-            )
-
-            self.jenkins_leader_task.default_container.add_mount_points(
-                ecs.MountPoint(
-                    container_path='/var/jenkins_home',
-                    source_volume="efs_mount",
-                    read_only=False
-                )
-            )
-
-            self.jenkins_leader_task.default_container.add_port_mappings(
-                ecs.PortMapping(
-                    container_port=8080,
-                    host_port=8080
-                )
-            )
-
-            self.jenkins_leader_service = ecs.Ec2Service(
-                self, "EC2leaderService",
-                task_definition=self.jenkins_leader_task,
-                cloud_map_options=ecs.CloudMapOptions(name="leader", dns_record_type=sd.DnsRecordType('A')),
-                desired_count=1,
-                min_healthy_percent=0,
-                max_healthy_percent=100,
-                enable_ecs_managed_tags=True,
-                cluster=self.cluster.cluster,
-            )
-
-            self.target_group = self.listener.add_targets(
-                "JenkinsleaderTarget",
-                port=80,
-                targets=[
-                    self.jenkins_leader_service.load_balancer_target(
-                        container_name=self.jenkins_leader_task.default_container.container_name,
-                        container_port=8080,
-                    )
-                ],
-                deregistration_delay=Duration.seconds(10)
-            )
+        self.jenkins_leader_service = self.jenkins_leader_service_main.service
+        self.jenkins_leader_task = self.jenkins_leader_service.task_definition
 
         # Opening port 5000 for leader <--> worker communications
         self.jenkins_leader_service.task_definition.default_container.add_port_mappings(
